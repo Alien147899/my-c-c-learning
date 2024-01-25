ab5fcbb03496ae603bb7b2621b8e5919e8323de0
Sheldon <95955412+Alien147899@users.noreply.github.com>
2024-01-25 17:09:26 +0800

    2_string_array.c

--- string/2_string_array.c
+++ string/2_string_array.c
@@ -3,7 +3,7 @@
 int main()
 {
     const char* str = "fuck you"; //指针str指向 "fuc..."这个字符串，这样赋值不能修改里面的字符
-    char word[] = "retard";//为什么这种赋值方式会比上面的好？------
+    char word[] = "retard";//为什么这种赋值方式会比上面的好？------（未找到答案，总之程序员一般给数组赋值字符串不会用上面的方式）
     word[0] = 'a';
     printf("%s\n", word);
     printf("%s\n", str);
